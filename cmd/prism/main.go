9f61334ddc8b748bd5765654eb51a01df2bf5e50
HT4w5 <ht4w5x@gmail.com>
2026-06-16 17:28:56 +0800

    feat(cmd): refactor entrypoint

--- cmd/prism/main.go
+++ cmd/prism/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"os"
 	"os/signal"
@@ -10,31 +9,81 @@ import (
 	"time"
 
 	"github.com/openana/prism/pkg/config"
+	"github.com/openana/prism/pkg/meta"
 	"github.com/openana/prism/pkg/server"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	if !Main() {
-		os.Exit(1)
-	}
+	cobra.CheckErr(rootCmd.Execute())
 }
 
-func Main() bool {
+var rootCmd = &cobra.Command{
+	Use:           "prism",
+	Short:         "Prism mirror gateway",
+	SilenceUsage:  true,
+	SilenceErrors: true,
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print version information",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(meta.VersionString)
+	},
+}
+
+var runCmd = &cobra.Command{
+	Use:   "run",
+	Short: "Run the prism server",
+	RunE:  runServer,
+}
+
+func init() {
+	rootCmd.AddCommand(runCmd)
+	rootCmd.AddCommand(versionCmd)
+
+	runCmd.Flags().StringP("config", "c", "config.yaml", "path to YAML config file")
+	runCmd.Flags().StringP("log-level", "l", "", "override log.level")
+	runCmd.Flags().String("log-output", "", "override log.output")
+	runCmd.Flags().String("access-log-level", "", "override access_log.level")
+	runCmd.Flags().String("access-log-output", "", "override access_log.output")
+}
+
+func runServer(cmd *cobra.Command, args []string) error {
 	defer initProfiles()()
 
-	configPath := flag.String("config", "config.yaml", "path to YAML config file")
-	flag.Parse()
+	configPath, _ := cmd.Flags().GetString("config")
 
-	cfg, err := config.Load(*configPath)
+	cfg, err := config.Load(configPath)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to load config: %v\n", err)
-		return false
+		return fmt.Errorf("failed to load config: %w", err)
+	}
+
+	// Print version string
+	fmt.Println(meta.VersionString)
+
+	// Apply config overrides
+	if cmd.Flags().Changed("log-level") {
+		val, _ := cmd.Flags().GetString("log-level")
+		cfg.Log.Level = val
+	}
+	if cmd.Flags().Changed("log-output") {
+		val, _ := cmd.Flags().GetString("log-output")
+		cfg.Log.Output = val
+	}
+	if cmd.Flags().Changed("access-log-level") {
+		val, _ := cmd.Flags().GetString("access-log-level")
+		cfg.AccessLog.Level = val
+	}
+	if cmd.Flags().Changed("access-log-output") {
+		val, _ := cmd.Flags().GetString("access-log-output")
+		cfg.AccessLog.Output = val
 	}
 
 	srv, cleanup, err := server.InitializeServer(cfg)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to initialize server: %v\n", err)
-		return false
+		return fmt.Errorf("failed to initialize server: %w", err)
 	}
 	defer cleanup()
 
@@ -42,8 +91,7 @@ func Main() bool {
 	defer cancel()
 
 	if err := srv.Run(ctx); err != nil {
-		fmt.Fprintf(os.Stderr, "server run error: %v\n", err)
-		return false
+		return fmt.Errorf("server run error: %w", err)
 	}
 
 	sigCh := make(chan os.Signal, 1)
@@ -57,5 +105,5 @@ func Main() bool {
 		fmt.Fprintf(os.Stderr, "server stop error: %v\n", err)
 	}
 
-	return true
+	return nil
 }
--- go.mod
+++ go.mod
@@ -22,12 +22,15 @@ require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
 	github.com/golang/snappy v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/klauspost/compress v1.18.6 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/savsgio/gotils v0.0.0-20240704082632-aef3928b8a38 // indirect
+	github.com/spf13/cobra v1.10.2 // indirect
+	github.com/spf13/pflag v1.0.9 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 	golang.org/x/sys v0.41.0 // indirect
